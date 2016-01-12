2b7dfc415d54b35b3b2be68508cbe27fbf3afe63
Alisson Zampietro (Casa) <alisson.zampietro@gmail.com>
2016-01-12 12:55:39 -0200

    Alterações no layout

--- index.php
+++ index.php
@@ -1,53 +1,59 @@
-<html>
-<head>
-<meta http-equiv="Content-Language" content="pt-br">
-<meta http-equiv="Content-Type" content="text/html; charset=windows-1252">
-<title>Controle de Leds com Arduino</title>
-</head>
-<body>
-<div align="center">
-    <table border="0" width="15%" cellpadding="0">
-        <tr></tr><tr><td>
- 
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="utf-8" />
+        <link rel="stylesheet" type="text/css" href="assets/css/style.css" />
+        <title>Controle de Leds com Arduino</title>
+    </head>
+    <body>
+        <section class="main">
+            <h1>Painel de controle de Luz</h1>
 
-<form method="POST" action="arduino.php">
-<p>
-    <input type="hidden" value="Amarelo" name="estado" >
-    <input type="submit" value="Ligar Led Amarelo" name="amarelo">
-</p>
-</form>
-    </td></tr><tr>
- 
+            <div class="luz">
+                <p class="title vermelho">Vermelho</p>
+                <div class="onoffswitch">
+                    <input type="checkbox" name="onoffswitch" class="onoffswitch-checkbox" id="myonoffswitch">
+                    <label class="onoffswitch-label" for="myonoffswitch">
+                        <span class="onoffswitch-inner"></span>
+                        <span class="onoffswitch-switch"></span>
+                    </label>
+                </div> 
+            </div>
+            
+            <div class="luz">
+                <p class="title amarelo">Amarelo</p>
+                <div class="onoffswitch">
+                    <input type="checkbox" name="onoffswitch" class="onoffswitch-checkbox" id="myonoffswitch1">
+                    <label class="onoffswitch-label" for="myonoffswitch1">
+                        <span class="onoffswitch-inner"></span>
+                        <span class="onoffswitch-switch"></span>
+                    </label>
+                </div>    
+            </div>
+            
+            <div class="luz">
+                <p class="title azul">Azul</p>
+                <div class="onoffswitch">
+                    <input type="checkbox" name="onoffswitch" class="onoffswitch-checkbox" id="myonoffswitch2">
+                    <label class="onoffswitch-label" for="myonoffswitch2">
+                        <span class="onoffswitch-inner"></span>
+                        <span class="onoffswitch-switch"></span>
+                    </label>
+                </div>
+            </div>
 
- 
-<form method="POST" action="arduino.php">
- 
-    <input type="hidden" value="Azul" name="estado" >
-    <td><input type="submit" value="    Ligar Led Azul    " name="azul">
-</td>
-</form>
-        </tr><tr><td>
- 
+            <div class="luz">
+                <p class="title verde">Verde</p>
+                <div class="onoffswitch">
+                    <input type="checkbox" name="onoffswitch" class="onoffswitch-checkbox" id="myonoffswitch3">
+                    <label class="onoffswitch-label" for="myonoffswitch3">
+                        <span class="onoffswitch-inner"></span>
+                        <span class="onoffswitch-switch"></span>
+                    </label>
+                </div>
+            </div>
 
- 
-<form method="POST" action="arduino.php">
-<p>
-    <input type="hidden" value="Laranja" name="estado" >
-    <input type="submit" value="  Ligar Led Laranja" name="laranja">
-</p>
-</form>
-        </td></tr><tr>
- 
-
- 
-<form method="POST" action="arduino.php">
- 
-    <input type="hidden" value="Verde" name="estado" >
-    <td><input type="submit" value="   Ligar Led Verde   " name="verde">
-</td>
-</form>
-    </tr>
-    </table>
-</div>
-</body>
+        </section>
+       
+    </body>
 </html>
